828a745042f1d871275fd14681d54582ec6617ae
Eric Curtin <ecurtin@redhat.com>
2025-01-15 20:33:26 +0000

    Use unique_ptr
    Signed-off-by: Eric Curtin <ecurtin@redhat.com>

--- llama-run-cli.cpp
+++ llama-run-cli.cpp
@@ -7,28 +7,21 @@
 
 #include "linenoise.h"
 
-struct c_str {
-    const char * data = nullptr;
-
-    ~c_str() { free((void *) data); }
-};
-
 int main(int, char **) {
     while (1) {
         static const char * prompt_prefix = "> ";
-        c_str               line;
-        line.data = linenoise(prompt_prefix);
-        if (line.data == NULL) {
+        std::unique_ptr<char, decltype(&free)> line(const_cast<char *>(linenoise(prompt_prefix)), free);
+        if (!line) {
             break;
         }
 
-        std::string user_input = line.data;
+        std::string user_input = line.get();
         if (user_input.empty()) {
             continue;
         }
         /* Do something with the string. */
-        printf("\033[33mecho: '%s'\033[0m\n", line.data);
-        linenoiseHistoryAdd(line.data); /* Add to the history. */
+        printf("\033[33mecho: '%s'\033[0m\n", line.get());
+        linenoiseHistoryAdd(line.get()); /* Add to the history. */
     }
 
     return 0;

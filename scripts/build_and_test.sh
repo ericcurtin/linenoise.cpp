c1dd36756209bb9777ab07f921b087c17cbb98ee
Eric Curtin <ecurtin@redhat.com>
2025-01-15 13:06:52 +0000

    Remove spurious characters
    Signed-off-by: Eric Curtin <ecurtin@redhat.com>

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -3,14 +3,14 @@
 main() {
   set -exu -o pipefail
   export CC=gcc
-$ export CXX=g++
-$ cmake .
+  export CXX=g++
+  cmake .
   make -j $(nproc)
 
   git clean -fdx
   export CC=clang
-$ export CXX=clang++
-$ cmake .
+  export CXX=clang++
+  cmake .
   make -j $(nproc)
 }
 
